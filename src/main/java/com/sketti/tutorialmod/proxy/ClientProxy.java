83f17e814d935e840e5e91711a2b101f7522361e
Mazardoushaterial <mazardoushaterial@gmail.com>
2020-12-20 17:32:39 -0500

    Part 2 complete.

---
+++ src/main/java/com/sketti/tutorialmod/proxy/ClientProxy.java
@@ -0,0 +1,6 @@
+package com.sketti.tutorialmod.proxy;
+
+public class ClientProxy extends CommonProxy
+{
+	
+}
---
+++ src/main/java/com/sketti/tutorialmod/proxy/CommonProxy.java
@@ -0,0 +1,5 @@
+package com.sketti.tutorialmod.proxy;
+
+public class CommonProxy {
+
+}
