bfa9f4420ec9196b6b34be77c57fdfc3d36acba3
mazardoushaterial <mazardoushaterial@gmail.com>
2020-12-20 22:22:49 -0500

    Part 8 Complete

--- src/main/java/com/sketti/tutorialmod/init/ModItems.java
+++ src/main/java/com/sketti/tutorialmod/init/ModItems.java
@@ -1,18 +1,29 @@
 package com.sketti.tutorialmod.init;
+import net.minecraftforge.common.util.EnumHelper;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import com.sketti.tutorialmod.items.ItemBase;
+import com.sketti.tutorialmod.items.tools.ToolSword;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraft.item.ItemSword;
 
 public class ModItems 
 {
+
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
+	//Materials
+	public static final ToolMaterial MATERIAL_RUBY = EnumHelper.addToolMaterial("material_ruby",3,250,8.0f,3.0f,10);
+
+	//Items
 	public static final Item RUBY = new ItemBase("ruby");
 	
+	//Tools
+	public static final ItemSword RUBY_SWORD = new ToolSword("ruby_sword", MATERIAL_RUBY);
 	
 	
 }
