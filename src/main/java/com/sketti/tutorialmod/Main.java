d3bfac58e1e8acf5bccdf4f7c797865f9868c33d
mazardoushaterial <mazardoushaterial@gmail.com>
2020-12-21 19:39:14 -0500

    Part 10 Complete

--- src/main/java/com/sketti/tutorialmod/Main.java
+++ src/main/java/com/sketti/tutorialmod/Main.java
@@ -2,6 +2,7 @@ package com.sketti.tutorialmod;
 import com.sketti.tutorialmod.init.ModRecipes;
 import com.sketti.tutorialmod.proxy.CommonProxy;
 import com.sketti.tutorialmod.util.Reference;
+import com.sketti.tutorialmod.world.ModWorldGen;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -10,6 +11,7 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
 public class Main 
@@ -24,7 +26,7 @@ public class Main
 	@EventHandler
 	public static void PreInit(FMLPreInitializationEvent event)
 	{
-		
+		GameRegistry.registerWorldGenerator(new ModWorldGen(), 3);
 	}
 	
 	@EventHandler
--- src/main/java/com/sketti/tutorialmod/init/ModBlocks.java
+++ src/main/java/com/sketti/tutorialmod/init/ModBlocks.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.sketti.tutorialmod.blocks.BlockBase;
 import com.sketti.tutorialmod.blocks.RubyBlock;
+import com.sketti.tutorialmod.blocks.RubyOre;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -14,5 +15,5 @@ public class ModBlocks
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	
 	public static final Block RUBY_BLOCK = new RubyBlock("ruby_block", Material.IRON);
-	
+	public static final Block RUBY_ORE = new RubyOre("ruby_ore",Material.ROCK);
 }
